e96cf7814d145431c0c7d40dd6e9f1d201713cb5
MK <krawiecmag@gmail.com>
2026-06-06 18:10:34 +0200

    offers fixes

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -1,4 +1,6 @@
+import { HtmlParser } from '@angular/compiler';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-offers',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.scss',
 })
-export class OffersComponent {}
+export class OffersComponent {
+}
